877741d2aa13dc8126d05aa99bb2d4e37b6709bc
Grant Heffernan <heffergm@gmail.com>
2013-09-26 16:46:01 -0400

    add depends

--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,10 @@ description      'Installs/Configures minutely_mapnik'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.0.1'
 
+%w{ postgiswrap osmosis osm2pgsql }.each do |dep|
+  depends dep
+end
+
 %w{ ubuntu }.each do |os|
     supports os
 end
