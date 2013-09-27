cf7038f00ad752296971b14172a9577db7c4d960
Grant Heffernan <heffergm@gmail.com>
2013-09-27 09:57:46 -0400

    update.sh fixes

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'grant@mapzen.com'
 license          'All rights reserved'
 description      'Installs/Configures minutely_mapnik'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
+version          '0.1.1'
 
 %w{ postgiswrap osmosis osm2pgsql }.each do |dep|
   depends dep
