409512747a327d157bced9f9cd7232b70a5af71b
Grant Heffernan <heffergm@gmail.com>
2013-09-27 10:03:38 -0400

    updates

--- recipes/config.rb
+++ recipes/config.rb
@@ -8,12 +8,16 @@
 #
 
 template "#{node[:minutely_mapnik][:basedir]}/bin/update.sh" do
+  owner 'root'
+  group 'root'
   action :create
   source 'update.sh.erb'
   mode 0755
 end
 
 cookbook_file "#{node[:minutely_mapnik][:basedir]}/bin/configuration.txt" do
+  owner 'postgres'
+  group 'postgres'
   action :create
   source 'configuration.txt'
   mode 0644
